c89de868ac93332b0857d8b6cab3cef43e27a2b4
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-06-25 21:35:45 +0200

    Encapsulamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,6 +19,10 @@ public class Program {
 //        int quantity = sc.nextInt();
 
         Product product = new Product(name, price);
+        product.setName("Computer");
+        System.out.println("Updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Updated price: " + product.getPrice());
 
         System.out.println();
         System.out.println("Product data: " + product);
@@ -27,6 +31,7 @@ public class Program {
         System.out.println("Enter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
         product.addProducts(quantity);
+
 
         System.out.println("Updated data: " + product);
 
